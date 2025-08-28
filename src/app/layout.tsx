72d9c6366ea66bbfb9df86b1c9730e45c4a886c8
Oneil Smiley <admin@neilussolutions.com>
2025-08-28 22:41:24 +0000

    Nothing is updating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: 'SwiftRoute',
@@ -37,5 +38,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-import { Toaster } from "@/components/ui/toaster";
