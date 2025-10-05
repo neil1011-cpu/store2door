0fe679d53d2f1555abf5b2443594e0d6b812d82b
Oneil Smiley <admin@neilussolutions.com>
2025-10-05 03:52:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "swiftroute-3230b",
+  "appId": "1:196006185725:web:7b7be99f2e15ca1af68c94",
+  "apiKey": "AIzaSyCxZ7fHM0GTfBtkyxaAhotzDw5udr7lFvQ",
+  "authDomain": "swiftroute-3230b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196006185725"
+};
